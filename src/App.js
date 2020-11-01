9e13d03bb1453fac7495637a4205ae91f310a220
Nader Shbib <nader_shbib@hotmail.com>
2020-11-01 14:13:08 -0800

    ok

--- src/App.js
+++ src/App.js
@@ -5,22 +5,7 @@ import axios from 'axios';
 
 function App() {
 
-   const [users,setUsers] = useState([]);
-
-      
-
-   useEffect(()=>{
-         
-           fetch('https://reqres.in/api/users?page=2')
-           .then(res=>res.json())
-           .then(res=>setUsers(res.data))
-           .catch(err=>alert(err.message))
-         
-        },[])
-        
-        
-        
-        console.log(users)
+    
 
   return (
     <div className="app">
