5bba81bf3d73ff6094f8f438d0589487f745ec6f
Nader Shbib <nader_shbib@hotmail.com>
2020-11-01 09:00:06 -0800

    added easily

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 
 import './App.css';
+import React,{useEffect, useState} from 'react'
+import axios from 'axios';
 
 function App() {
 
+   const [users,setUsers] = useState([]);
 
+      
 
-
-
+   useEffect(()=>{
+         
+           fetch('https://reqres.in/api/users?page=2')
+           .then(res=>res.json())
+           .then(res=>setUsers(res.data))
+           .catch(err=>alert(err.message))
+         
+        },[])
+        
+        
+        
+        console.log(users)
 
   return (
     <div className="app">
   
         
- 
+      
       
 
     </div>
